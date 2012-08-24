a6cd5a8aae49307b0e0c08480c6f0e7ce5afe2c1
Daniel Freedman <dfreedm@google.com>
2012-08-23 17:03:13 -0700

    base events from mouse and touch, normalized

---
+++ src/gesture.js
@@ -0,0 +1,16 @@
+/*
+    Events:
+        - pointerzoom
+        - pointerrotate
+*/
+(function() {
+    var dispatcher = sk.dispatcher;
+    var gesture = {
+        pointerdown: function(inEvent) {
+        },
+        pointerup: function(inEvent) {
+        },
+        pointermove: function(inEvent) {
+        }
+    };
+})();
