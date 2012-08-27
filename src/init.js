b571c5d3c374d34e0933a2b0d9619d394206d22a
Daniel Freedman <dfreedm@google.com>
2012-08-27 12:24:23 -0700

    make init conform to how the other modules look

--- src/init.js
+++ src/init.js
@@ -1,28 +1,5 @@
-(function() {
-  window.PointerEventShim = {
-    clone: function(inSink, inSource) {
-      var p$ = [].slice.call(arguments, 1);
-      for (var i=0, p; p=p$[i]; i++) {
-        if (p) {
-          var g, s;
-          for (var n in p) {
-            //inSink[n] = p[n];
-            /* fixme: sigh, copy getters/setters  */
-            g = p.__lookupGetter__(n), s = p.__lookupSetter__(n);
-            if (g) {
-              inSink.__defineGetter__(n, g);
-            } else {
-              inSink[n] = p[n];
-            }
-            if (s) {
-              inSink.__defineSetter__(n, s);
-            }
-          }
-        }
-      }
-      return inSink;
-    },
-  };
+(function(scope) {
+  // Function bind is required, iOS is missing it :(
   if (!Function.prototype.bind) {
     Function.prototype.bind = function(scope) {
       var _this = this;
@@ -31,4 +8,26 @@
       }
     }
   }
-})();
+  scope = scope || {};
+  scope.clone = function(inSink, inSource) {
+    var p$ = [].slice.call(arguments, 1);
+    for (var i=0, p; p=p$[i]; i++) {
+      if (p) {
+        var g, s;
+        for (var n in p) {
+          // copy getters/setters
+          g = p.__lookupGetter__(n), s = p.__lookupSetter__(n);
+          if (g) {
+            inSink.__defineGetter__(n, g);
+          } else {
+            inSink[n] = p[n];
+          }
+          if (s) {
+            inSink.__defineSetter__(n, s);
+          }
+        }
+      }
+    }
+    return inSink;
+  };
+})(window.PointerEventShim);
