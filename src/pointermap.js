606f0b4d9cc60740a84d40a8745a6345dda8d115
Daniel Freedman <dfreedm@google.com>
2012-09-05 16:37:09 -0700

    Doc pointermap more, remove updatePointer (broken idea)

--- src/pointermap.js
+++ src/pointermap.js
@@ -4,17 +4,28 @@
  * license that can be found in the LICENSE file.
  */
 
+/*
+ * This module implements an ordered list of pointer states
+ * Each pointer object here has three properties:
+ *  - id: the id of the pointer
+ *  - event: the source event of the pointer, complete with positions
+ *  - state: extra state the pointer needs to carry
+ *
+ * The ordering of the pointers is from oldest pointer to youngest pointer,
+ * which allows for multi-pointer gestures to not rely on the actual ids
+ * imported from the source events.
+ */
 (function(scope) {
   var pointermap = {
     // pointers is an array of pointer states, ordered from oldest to youngest
     pointers: [],
-    addPointer: function(inPointer) {
-      this.pointers.push(inPointer);
+    addPointer: function(inId, inSrcEvent, inState) {
+      var p = {id: inId, event: inSrcEvent, state: inState};
+      this.pointers.push(p);
     },
     removePointer: function(inId) {
-      var p = this.getPointerById(inId);
-      if (p) {
-        var i = this.pointers.indexOf(p);
+      var i = this.getPointerIndex(inId);
+      if (i > -1) {
         this.pointers.splice(i, 1);
       }
     },
@@ -31,10 +42,6 @@
     },
     getPointerList: function() {
       return this.pointers;
-    },
-    updatePointer: function(inId, inPointer) {
-      var i = this.getPointerIndex(inId);
-      this.pointers[i] = inPointer;
     }
   };
   scope.pointermap = pointermap;
