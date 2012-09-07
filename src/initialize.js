94711d3d209cac71b05a70776c2d8afea33021aa
Daniel Freedman <dfreedm@google.com>
2012-09-07 11:49:48 -0700

    Fixes for firefox compatiblity
    - All properties on events in firefox are getters to native code, complain loudly
      when accessed from a cloned event
    
    - Use DOM Level 3 events (event.buttons) when possible, which is broken in these
      instances
    
    - Sample should have some charset

--- src/initialize.js
+++ src/initialize.js
@@ -18,18 +18,8 @@
     var p$ = [].slice.call(arguments, 1);
     for (var i=0, p; p=p$[i]; i++) {
       if (p) {
-        var g, s;
         for (var n in p) {
-          // copy getters/setters
-          g = p.__lookupGetter__(n), s = p.__lookupSetter__(n);
-          if (g) {
-            inSink.__defineGetter__(n, g);
-          } else {
             inSink[n] = p[n];
-          }
-          if (s) {
-            inSink.__defineSetter__(n, s);
-          }
         }
       }
     }
