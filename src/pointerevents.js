00f1f6f2927fc49e5cdb56db766562716ea2d941
Daniel Freedman <dfreedm@google.com>
2012-09-10 14:58:07 -0700

    Allow for loading from inside a web component
    Walk up to the root to find url, don't depend on document's location

--- src/pointerevents.js
+++ src/pointerevents.js
@@ -5,18 +5,35 @@
  */
 (function() {
   var thisFile = 'pointerevents.js';
-  var source = '', base = '';
+  var libLocation = '';
+  /*
+   * if we are loaded inside a component, we need to know the relative path from
+   * that location, not the document
+   */
+  var baseUrlFromNode = function(inNode) {
+    var n = inNode, p;
+    while ((p = n.parentNode)) {
+      n = p;
+    }
+    return (n && (n.URL || n.name)) || '';
+  };
+  var appendScriptPath = function(inBasePath, inRelPath) {
+    // include last slash as well
+    var ls = inBasePath.lastIndexOf('/') + 1;
+    return inBasePath.slice(0, ls) + inRelPath;
+  };
+  var require = function(inSrc) {
+    document.write('<script src="' + libLocation + inSrc + '"></script>');
+  };
   var s$ = document.querySelectorAll('script[src]');
   Array.prototype.forEach.call(s$, function(s) {
     var src = s.getAttribute('src');
     if (src.slice(-thisFile.length) == thisFile) {
-      source = s;
-      base = src.slice(0, -thisFile.length);
+      var source = baseUrlFromNode(s);
+      var base = src.slice(0, -thisFile.length);
+      libLocation = appendScriptPath(source, base);
     }
   });
-  var require = function(inSrc) {
-    document.write('<script src="' + base + inSrc + '"></script>');
-  };
   [
     'initialize.js',
     'pointermap.js',
