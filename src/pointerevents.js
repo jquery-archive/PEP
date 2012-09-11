75970cc3bb729261f093aca20a06fdafea6cb752
Daniel Freedman <dfreedm@google.com>
2012-09-10 17:10:25 -0700

    Remove some cruft
    Use Array#some to short circuit from loop

--- src/pointerevents.js
+++ src/pointerevents.js
@@ -6,32 +6,15 @@
 (function() {
   var thisFile = 'pointerevents.js';
   var libLocation = '';
-  /*
-   * if we are loaded inside a component, we need to know the relative path from
-   * that location, not the document
-   */
-  var baseUrlFromNode = function(inNode) {
-    var n = inNode, p;
-    while ((p = n.parentNode)) {
-      n = p;
-    }
-    return (n && (n.URL || n.name)) || '';
-  };
-  var appendScriptPath = function(inBasePath, inRelPath) {
-    // include last slash as well
-    var ls = inBasePath.lastIndexOf('/') + 1;
-    return inBasePath.slice(0, ls) + inRelPath;
-  };
   var require = function(inSrc) {
     document.write('<script src="' + libLocation + inSrc + '"></script>');
   };
   var s$ = document.querySelectorAll('script[src]');
-  Array.prototype.forEach.call(s$, function(s) {
+  Array.prototype.some.call(s$, function(s) {
     var src = s.getAttribute('src');
     if (src.slice(-thisFile.length) == thisFile) {
-      var source = baseUrlFromNode(s);
-      var base = src.slice(0, -thisFile.length);
-      libLocation = appendScriptPath(source, base);
+      libLocation = src.slice(0, -thisFile.length);
+      return true;
     }
   });
   [
