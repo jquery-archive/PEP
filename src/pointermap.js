6c4da6777da38446268f7e6a7a0baf765af1a99c
Daniel Freedman <dfreedm@google.com>
2012-10-01 16:28:38 -0700

    Event property in PointerMap was unused

--- src/pointermap.js
+++ src/pointermap.js
@@ -18,20 +18,20 @@
  * objects off of the pointer in the pointermap. This information will be
  * preserved until the pointer is removed from the pointermap.
  *
- * This module is implementation specific, and should not be part of any spec.
+ * This module is implementation specific.
  */
 (function(scope) {
   var pointermap = {
     pointers: [],
-    addPointer: function(inId, inSrcEvent) {
-      var p = {id: inId, event: inSrcEvent};
+    addPointer: function(inId) {
+      var p = {id: inId};
       this.pointers.push(p);
       return p;
     },
     removePointer: function(inId) {
       var i = this.getPointerIndex(inId);
       if (i > -1) {
-        return this.pointers.splice(i, 1);
+        return this.pointers.splice(i, 1)[0];
       }
     },
     getPointerById: function(inId) {
