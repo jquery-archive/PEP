68ffba789c7fdf3d139abff1fff2e0f147bc1b5d
Daniel Freedman <dfreedm@google.com>
2013-02-25 14:50:04 -0800

    up testacular timeout to 1 minute

--- testacular.conf.js
+++ testacular.conf.js
@@ -68,7 +68,7 @@ browsers = ['Chrome', 'ChromeCanary', 'Firefox', 'Safari'];
 
 
 // If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+captureTimeout = 60000;
 
 
 // Continuous Integration mode
