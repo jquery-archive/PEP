09e616ba681c01e528724f9b38e60eecf875ecd6
Daniel Freedman <dfreedm@google.com>
2013-08-27 11:08:23 -0700

    update sidetable

--- src/sidetable.js
+++ src/sidetable.js
@@ -1,20 +1,19 @@
 /*
- * Copyright 2013 The Polymer Authors. All rights reserved.
+ * Copyright 2012 The Polymer Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file.
  */
 
 // SideTable is a weak map where possible. If WeakMap is not available the
 // association is stored as an expando property.
-(function(scope) {
-  var SideTable;
-  // TODO(dfreedman): WeakMap does not allow for Events to be keys in Firefox
-  if (typeof WeakMap !== 'undefined' && navigator.userAgent.indexOf('Firefox/') < 0) {
-    SideTable = WeakMap;
-  } else {
+var SideTable;
+// TODO(arv): WeakMap does not allow for Node etc to be keys in Firefox
+if (typeof WeakMap !== 'undefined' && navigator.userAgent.indexOf('Firefox/') < 0) {
+  SideTable = WeakMap;
+} else {
+  (function() {
     var defineProperty = Object.defineProperty;
-    var hasOwnProperty = Object.hasOwnProperty;
-    var counter = new Date().getTime() % 1e9;
+    var counter = Date.now() % 1e9;
 
     SideTable = function() {
       this.name = '__st' + (Math.random() * 1e9 >>> 0) + (counter++ + '__');
@@ -22,15 +21,20 @@
 
     SideTable.prototype = {
       set: function(key, value) {
-        defineProperty(key, this.name, {value: value, writable: true});
+        var entry = key[this.name];
+        if (entry && entry[0] === key)
+          entry[1] = value;
+        else
+          defineProperty(key, this.name, {value: [key, value], writable: true});
       },
       get: function(key) {
-        return hasOwnProperty.call(key, this.name) ? key[this.name] : undefined;
+        var entry;
+        return (entry = key[this.name]) && entry[0] === key ?
+            entry[1] : undefined;
       },
       delete: function(key) {
         this.set(key, undefined);
       }
-    };
-  }
-  scope.SideTable = SideTable;
-})(window.PointerEventsPolyfill);
+    }
+  })();
+}
