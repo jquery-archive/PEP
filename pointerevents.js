46ea56932f0b7b4b2752f489be71e4061e191454
Daniel Freedman <dfreedm@google.com>
2013-10-09 16:48:16 -0700

    Put pointermap back, remove sidetable reference

--- pointerevents.js
+++ pointerevents.js
@@ -11,7 +11,7 @@
     'src/boot.js',
     'src/touch-action.js',
     'src/PointerEvent.js',
-    'src/sidetable.js',
+    'src/pointermap.js',
     'src/dispatcher.js',
     'src/installer.js',
     'src/mouse.js',
