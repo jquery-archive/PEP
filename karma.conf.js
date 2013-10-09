b094f645c39896467466932206d87ddaf5fccd6f
Daniel Freedman <dfreedm@google.com>
2013-10-09 16:31:54 -0700

    fix karma paths for WeakMap

--- karma.conf.js
+++ karma.conf.js
@@ -2,26 +2,26 @@ module.exports = function(karma) {
   var common = require('../tools/test/karma-common.conf.js');
   karma.set(common.mixin_common_opts(karma, {
     // base path, that will be used to resolve files and exclude
-    basePath: '',
+    basePath: '../',
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules/chai/chai.js',
-      'node_modules/chai-spies/chai-spies.js',
-      'src/boot.js',
-      'src/PointerEvent.js',
-      'src/pointermap.js',
-      'src/sidetable.js',
-      'src/dispatcher.js',
-      'src/installer.js',
-      'src/mouse.js',
-      'src/touch.js',
-      'src/ms.js',
-      'src/platform-events.js',
-      'src/capture.js',
-      'tests/setup.js',
-      'tests/karma-setup.js',
-      'tests/!(setup|karma-setup).js'
+      'PointerEvents/node_modules/chai/chai.js',
+      'PointerEvents/node_modules/chai-spies/chai-spies.js',
+      'WeakMap/weakmap.js',
+      'PointerEvents/src/boot.js',
+      'PointerEvents/src/PointerEvent.js',
+      'PointerEvents/src/pointermap.js',
+      'PointerEvents/src/dispatcher.js',
+      'PointerEvents/src/installer.js',
+      'PointerEvents/src/mouse.js',
+      'PointerEvents/src/touch.js',
+      'PointerEvents/src/ms.js',
+      'PointerEvents/src/platform-events.js',
+      'PointerEvents/src/capture.js',
+      'PointerEvents/tests/setup.js',
+      'PointerEvents/tests/karma-setup.js',
+      'PointerEvents/tests/!(setup|karma-setup).js'
     ],
   }));
 };
