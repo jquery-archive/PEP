ba001fdf014ae98a95576c9cdf9b05752ee2de00
Daniel Freedman <dfreedm@google.com>
2014-02-19 14:17:22 -0800

    fix license headers

--- samples/paint/paint.js
+++ samples/paint/paint.js
@@ -1,3 +1,10 @@
+/**
+ * @license
+ * Copyright 2014 The Polymer Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 // shim layer with setTimeout fallback
 window.requestAnimFrame = (function(){
   return  window.requestAnimationFrame ||
