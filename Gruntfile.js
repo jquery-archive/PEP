461f452db52c0cb5f9054cfd11047a513dfee949
Alexander Schmitz <arschmitz@gmail.com>
2016-04-20 21:41:21 -0400

    Build: Bump grunt-selenium-standalone removing need for shrinkwrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,17 +57,20 @@ module.exports = function(grunt) {
       }
     },
     "selenium_standalone": {
+        options: {
+          stopOnExit: true
+        },
         pointerevents: {
-            seleniumVersion: '2.47.1',
+            seleniumVersion: '2.53.0',
             seleniumDownloadURL: 'http://selenium-release.storage.googleapis.com',
             drivers: {
                 chrome: {
-                  version: '2.2',
+                  version: '2.21',
                   arch: process.arch,
                   baseURL: 'http://chromedriver.storage.googleapis.com'
                 },
                 ie: {
-                  version: '2.47',
+                  version: '2.53',
                   arch: process.arch,
                   baseURL: 'http://selenium-release.storage.googleapis.com'
                 }
