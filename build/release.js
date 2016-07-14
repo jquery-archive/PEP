742be70fbeac27d34016cdb2ec1507bc7c73cf56
Scott González <scott.gonzalez@gmail.com>
2016-07-14 12:46:02 -0400

    Build: Update version references in the README during releases
    Fixes gh-249

--- build/release.js
+++ build/release.js
@@ -1,3 +1,6 @@
+var fs = require("fs");
+var path = require("path");
+
 module.exports = function(Release) {
 
 Release.define({
@@ -11,7 +14,17 @@ Release.define({
 
 	generateArtifacts: function(callback) {
 		Release.exec("grunt");
+
+		// Update version references in README.md
+		var readmePath = path.join(Release.dir.repo, "README.md");
+		var readme = fs.readFileSync(readmePath, "utf8").replace(
+			/pep\/\d+\.\d+\.\d+\/pep\.js/g,
+			"pep/" + Release.newVersion + "/pep.js"
+		);
+		fs.writeFileSync(readmePath, readme);
+
 		callback([
+			"README.md",
 			"dist/pep.js",
 			"dist/pep.min.js"
 		]);
